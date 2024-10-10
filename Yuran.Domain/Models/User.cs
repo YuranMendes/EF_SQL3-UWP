b59108b766f814f3791e6e8995b3723b9a82a524
Yuran Jorge Gomes Mendes <a44783@alunos.ipb.pt>
2024-10-10 15:14:14 +0100

    Criacao dos models da diretoria model

--- Yuran.Domain/Models/User.cs
+++ Yuran.Domain/Models/User.cs
@@ -5,13 +5,13 @@ namespace Yuran.Domain.Models
 {
     public class User : Entity
     {
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public string PostalCodeId { get; set; }
-        public PostalCode PostalCode { get; set; }
-        public ICollection<Permission> Permissions { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public string? PostalCodeId { get; set; }
+        public PostalCode? PostalCode { get; set; }
+        public ICollection<Permission>? Permissions { get; set; }
         //public ICollection<EntryProduct> EntryProducts { get; set; }
-        public ICollection<OutProduct> OutProducts { get; set; }
+        public ICollection<OutProduct>? OutProducts { get; set; }
 
         public override string ToString()
         {
